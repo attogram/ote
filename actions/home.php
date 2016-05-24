7c5a484c41133f2be41fa89cf0d5c200dd1dfd2f
David B <barberi.office@gmail.com>
2016-05-24 21:07:57 +0200

    design

--- actions/home.php
+++ actions/home.php
@@ -1,28 +1,24 @@
 <?php
 namespace Attogram;
 
-$this->page_header('OTE v1.0.0-dev');
+$this->page_header('Open Translation Engine ' . OTE_VERSION);
 
 ?>
 <div class="container">
-<h2>The Open Translation Engine v1.0.0-dev</h2>
-<p>The OTE is an open source, open content, multi-user
+<h2>The Open Translation Engine</h2>
+<p>
+ Welcome to the Open Translation Engine (OTE) v<?php print OTE_VERSION; ?>.
+ The OTE is an open source, open content, multi-user
  <nobr><strong>translation dictionary</strong></nobr>
- manager for the web.</p>
+ manager for the web.
+</p>
 
 <ul>
-  <li><a href="languages/">Languages</a></li>
-  <li><a href="words/">Words</a></li>
-  <li><a href="word2word/">Word2Word</a></li>
+  <li><a href="dictionary/">Dictionary</a></li>
+  <li><a href="export/">Export</a></li>
+  <li><a href="import/">Import</a></li>
 </ul>
 
-<ul>  
-  <li><a href="languages-admin/">Languages-Admin</a></li>  
-  <li><a href="words-admin/">Words-Admin</a></li>
-  <li><a href="word2word-admin/">Word2Word-Admin</a></li>
-  <li><a href="users-admin/">Users-Admin</a></li>
-</ul>
-  
 </div>
 <?php
 $this->page_footer();
