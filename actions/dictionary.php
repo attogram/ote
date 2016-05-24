f7f6b77f7a979766e1fc3346a74c410b598c5e01
David B <barberi.office@gmail.com>
2016-05-24 13:15:17 +0200

    start dictionary page

---
+++ actions/dictionary.php
@@ -0,0 +1,35 @@
+<?php
+namespace Attogram;
+
+# requires setup in config.php:  $config['depth']['dictionary'] = 4;
+
+if( !isset($this->uri[1]) || !$this->uri[1] ) {
+  // Please select Source Langauge Code 
+  $this->error404();
+}
+if( !isset($this->uri[2]) || !$this->uri[2] ) {
+  // Please select Target Language Code
+  $this->error404();
+}
+
+$s_code = $this->uri[1];
+$t_code = $this->uri[2];
+
+$langs = get_languages($this->sqlite_database);
+
+if( !isset($langs[$s_code]) ) {
+  // Source Language Code Not Found
+  $this->error404();
+}
+if( !isset($langs[$t_code]) ) {
+  // Target Language Code Not Found
+  $this->error404();
+}
+
+$title = $langs[$s_code] . ' to ' . $langs[$t_code] . ' Dictionary';
+
+$this->page_header($title . ' - OTE 1.0.0-dev');
+print '<div class="container"><h1>' . $title . '</h1>';
+
+print '</div>';
+$this->page_footer();
