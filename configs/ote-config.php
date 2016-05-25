c3826a9780bea1dd67a6f2680328f9f005f8297e
David B <barberi.office@gmail.com>
2016-05-25 23:46:52 +0200

    default title

--- configs/ote-config.php
+++ configs/ote-config.php
@@ -1,6 +1,8 @@
 <?php
 // Additional Attogram config setup for OTE
 
+$config['site_name'] = 'Open Translation Engine';
+
 $config['depth']['dictionary'] = 4; // Dictionary Depth
 
 $config['depth']['word'] = 5; // word viewer Depth
