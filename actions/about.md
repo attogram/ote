0aac40c902e55a1dc5ee9cdb8d66fc40a1e5c0f1
David B <barberi.office@gmail.com>
2016-05-26 14:17:40 +0200

    intro text

--- actions/about.md
+++ actions/about.md
@@ -1,4 +1,13 @@
-The Open Translation Engine
-==================
+Open Translation Engine (OTE)
+===
 
-about...
+The OTE is a **translation dictionary** manager for the web.
+
+The OTE source is **open source**, 
+it is dual licensed under the **MIT License**
+or the **GNU General Public License**,
+at your choosing.
+
+The OTE content is **open content**, 
+words and translations are 
+licened under the **Creative Commons Attribution-Share Alike license**, or similar.
--- actions/home.php
+++ actions/home.php
@@ -5,10 +5,13 @@ $this->page_header('Open Translation Engine v' . OTE_VERSION);
 
 ?>
 <div class="container">
-<h1>The Open Translation Engine</h1>
-<p>Welcome to the Open Translation Engine (OTE) version <?php print OTE_VERSION; ?>.</p>
-<p>The OTE is a multi-user <nobr><strong>translation dictionary</strong></nobr> manager for the web.</p>
-<p>The OTE is open source and open content.</p>
+<h1>Open Translation Engine (OTE)</h1>
+<p>The OTE is a <strong>translation dictionary</strong> manager for the web.</p>
+<p>The OTE source is <strong>open source</strong>, it is dual licensed under the 
+   <strong>MIT License</strong> or the <strong>GNU General Public License</strong>, 
+   at your choosing.</p>
+<p>The OTE content is <strong>open content</strong>, words and translations are 
+   licened under the <strong>Creative Commons Attribution-Share Alike license</strong>, or similar.</p>
 <ul>
   <li><a href="dictionary/">Dictionary list</a></li>
   <li><a href="word/">Word list</a></li>
