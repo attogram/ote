c505496b212cdf3bf9a3f3984f15f13a589bef49
David B <barberi.office@gmail.com>
2016-05-26 22:37:37 +0200

    prettyify

--- tables/word2word.sql
+++ tables/word2word.sql
@@ -1,8 +1,8 @@
 CREATE TABLE IF NOT EXISTS 'word2word' (
-  'id'     INTEGER PRIMARY KEY,
-  's_id'   INTEGER NOT NULL DEFAULT '0',
-  's_code' TEXT NOT NULL,
-  't_id'   INTEGER NOT NULL DEFAULT '0',
-  't_code' TEXT NOT NULL,
+  'id'      INTEGER PRIMARY KEY,
+  's_id'    INTEGER NOT NULL DEFAULT '0',
+  's_code'  TEXT NOT NULL,
+  't_id'    INTEGER NOT NULL DEFAULT '0',
+  't_code'  TEXT NOT NULL,
   unique(s_id,s_code,t_id,t_code)
 )
