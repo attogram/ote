71beec1d5e0510c87bd86964b7d58a0f7af234a2
David B <barberi.office@gmail.com>
2016-05-27 11:59:31 +0200

    do_import() into ote class, limit tests

--- actions/import.php
+++ actions/import.php
@@ -8,11 +8,28 @@ $default['t'] = '';
 $default['d'] = '=';
 
 if( $_POST ) {
-  do_import( $this->sqlite_database );
+    if( !isset($_POST['w']) || !$_POST['w'] ) {
+      $error[] = 'error - no word pairs found';
+    }
+    if( !isset($_POST['d']) || !$_POST['d'] ) {
+      $error[] = 'error - no deliminator set';
+    }
+    if( !isset($_POST['t']) || !$_POST['t'] ) {
+      $error[] = 'error - no target language code set';
+    }  
+    if( !isset($_POST['s']) || !$_POST['s'] ) {
+      $error[] = 'error - no source language code set';
+    }   
+    if( isset($error) ) {
+      print '<div class="container"><p>' . implode( $error, '<br />' ) . '</p></div>';
+    } else {
+      $ote = new ote($this->sqlite_database);
+      $ote->do_import( $_POST['w'], $_POST['d'], $_POST['s'], $_POST['t'], @$_POST['sn'], @$_POST['tn'] );
+    }
 }
 ?>
 <div class="container">
-  <h1>Import Word Pairs</h1>
+  <h1>Import Translations</h1>
   
   <form action="." method="POST">
   
@@ -57,137 +74,3 @@ if( $_POST ) {
 </div>
 <?php
 $this->page_footer();
-
-function do_import($db) {
-
-  if( !isset($_POST['w']) || !$_POST['w'] ) {
-    $error[] = 'error - no word pairs found';
-  }
-  if( !isset($_POST['d']) || !$_POST['d'] ) {
-    $error[] = 'error - no deliminator set';
-  }
-  if( !isset($_POST['t']) || !$_POST['t'] ) {
-    $error[] = 'error - no target language code set';
-  }  
-  if( !isset($_POST['s']) || !$_POST['s'] ) {
-    $error[] = 'error - no source language code set';
-  }   
-  if( isset($error) ) {
-    print '<div class="container"><p>' . implode( $error, '<br />' ) . '</p></div>';
-    return;
-  }
-  
-  $ote = new ote($db);
-  
-  $w = $_POST['w']; // List of word pairs
-
-  $d = $_POST['d']; // Deliminator
-  $d = str_replace('\t', "\t", $d); // allow real tabs
-
-  $s = trim($_POST['s']); // Source Language Code
-  $sn = $ote->get_language_name_from_code($s, $default=@$_POST['sn']);
-
-  $t = trim($_POST['t']); // Target Language Code
-  $tn = $ote->get_language_name_from_code($t, $default=@$_POST['tn']);
-
-  $lines = explode("\n", $w);
-  print '<div class="container">';
-  print 'Source Language: Code: <code>' . htmlentities($s) . '</code> Name: <code>' . htmlentities($sn) . '</code><br />';
-  print 'Target Language: Code: <code>' . htmlentities($t) . '</code> Name: <code>' . htmlentities($tn) . '</code><br />';
-  print 'Deliminator: <code>' . htmlentities($d) . '</code><br />';
-  print 'Lines: <code>' . sizeof($lines) . '</code><hr /><small>';
-
-  $line_count = 0;
-  $import_count = 0;
-  $error_count = 0;
-  $skip_count = 0;
-  $dupe_count = 0;
-  
-  foreach($lines as $line) {
-
-    set_time_limit(240);
-
-    $line_count++;
-    $line = trim($line);
-    
-    if( $line == '' ) {
-      //print '<p>Info: Line #' . $line_count . ': Blank line found. Skipping line</p>';
-      $skip_count++;
-      continue;
-    }
-    
-    if( preg_match('/^#/', $line) ) {
-      //print '<p>Info: Line #' . $line_count . ': Comment line found. Skipping line.</p>';
-      $skip_count++;
-      continue;
-    }
-
-    if( !preg_match('/' . $d . '/', $line) ) {
-      print '<p>Error: Line #' . $line_count . ': Deliminator (' .htmlentities($d) . ') Not Found. Skipping line.</p>';
-      $error_count++; $skip_count++;
-      continue;
-    }
-
-    $wp = explode($d, $line);
-
-    if( sizeof($wp) != 2 ) {
-      print '<p>Error: Line #' . $line_count . ': Malformed line.  Expecting 2 words, found ' . sizeof($wp) . ' words</p>';
-      $error_count++; $skip_count++;
-      continue;
-    }
-    
-    $sw = trim($wp[0]);
-    if( !$sw ) {
-      print '<p>Error: Line #' . $line_count . ': Malformed line.  Missing source word</p>';
-      $error_count++; $skip_count++;
-      continue;      
-    }
-    $tw = trim($wp[1]);
-    if( !$tw ) {
-      print '<p>Error: Line #' . $line_count . ': Malformed line.  Missing target word</p>';
-      $error_count++; $skip_count++;
-      continue;      
-    }
-    
-    $si = $ote->get_id_from_word($sw);
-    $ti = $ote->get_id_from_word($tw);
-    
-    $sql = 'INSERT INTO word2word (s_id, s_code, t_id, t_code) VALUES (:s_id, :s_code, :t_id, :t_code)';
-    $bind = array( 's_id'=>$si, 's_code'=>$s, 't_id'=>$ti, 't_code'=>$t);
-    $r = $db->queryb($sql, $bind);
-    if( !$r ) {
-      if( $db->db->errorCode() == '0000' ) {
-        //print '<p>Info: Line #' . $line_count . ': Duplicate.  Skipping line';
-        $error_count++; $dupe_count++; $skip_count++;
-        continue;
-      }
-      print '<p>Error: Line #' . $line_count . ': Database Insert Error.'
-      . ' sw: ' . htmlentities($sw)
-      . ' tw: ' . htmlentities($tw)
-      . ' Bind: ' . print_r($bind,1) 
-      . ' errorInfo: ' . print_r($db->db->errorInfo(),1) 
-      . '</p>';
-      $error_count++; $skip_count++;
-    } else {
-      $import_count++;
-      
-      if( $line_count % 100 == 0 ) {
-        print ' ' . $line_count . ' ';
-        @ob_flush(); flush();
-      } elseif( $line_count % 10 == 0 ) {
-        print '.';
-        @ob_flush(); flush();
-      }
-       
-    }
-
-  } // end foreach line
-  
-  print '</small><hr />';
-  print '<code>' . $import_count . '</code> word pairs imported.<br />';
-  print '<code>' . $error_count . '</code> errors.<br />';
-  print '<code>' . $dupe_count . '</code> duplicate lines.<br />';
-  print '<code>' . $skip_count . '</code> lines skipped.<br />';
-  print '</div>';
-  
-} // end do_import
--- functions/ote.php
+++ functions/ote.php
@@ -1,8 +1,7 @@
 <?php
 /*
-
-Open Translation Engine (OTE)
-
+ * Open Translation Engine (OTE)
+ * powered by Attogram Framework
 */
 namespace Attogram;
 
@@ -133,6 +132,8 @@ class ote {
    */
   function get_all_words() {
     $sql = 'SELECT word FROM word ORDER BY word';
+    $limit = ' LIMIT 0,10'; // dev
+    $sql .= $limit;
     return $this->db->query($sql);   
   }
 
@@ -169,9 +170,9 @@ class ote {
       $bind['s_code']=$s_code;
     }
 
-    $order = '';  // dev
-    $sql .= "$and $order";
-    $r_sql .= "$r_and $order";
+    $limit = ' LIMIT 0,10';  // dev
+    $sql .= "$and $limit";
+    $r_sql .= "$r_and $limit";
 
     $r = $this->db->query($sql, $bind);
     $r_r = $this->db->query($r_sql, $bind);
@@ -250,5 +251,154 @@ class ote {
     });
     return $array;
   }
+
+  /**
+   * do_import()
+   *
+   * @param string $w List of word pairs
+   * @param string $d Deliminator
+   * @param string $s Source Language Code
+   * @param string $t Target Language Code
+   * @param string $sn Optional. Source Language Name
+   * @param string $tn Optional. Target Language Name
+   *
+   */   
+  function do_import( $w, $d, $s, $t, $sn='', $tn='' ) {
+
+    //$w = $_POST['w']; // List of word pairs
+
+    //$d = $_POST['d']; // Deliminator
+    $d = str_replace('\t', "\t", $d); // allow real tabs
+
+    //$s = trim($_POST['s']); // Source Language Code
+    $sn = $this->get_language_name_from_code($s, $default=$sn);
+
+    //$t = trim($_POST['t']); // Target Language Code
+    $tn = $this->get_language_name_from_code($t, $default=$tn);
+
+    $lines = explode("\n", $w);
+    print '<div class="container">';
+    print 'Source Language: Code: <code>' . htmlentities($s) . '</code> Name: <code>' . htmlentities($sn) . '</code><br />';
+    print 'Target Language: Code: <code>' . htmlentities($t) . '</code> Name: <code>' . htmlentities($tn) . '</code><br />';
+    print 'Deliminator: <code>' . htmlentities($d) . '</code><br />';
+    print 'Lines: <code>' . sizeof($lines) . '</code><hr /><small>';
+
+    $line_count = 0;
+    $import_count = 0;
+    $error_count = 0;
+    $skip_count = 0;
+    $dupe_count = 0;
+    
+    foreach($lines as $line) {
+
+      set_time_limit(240);
+
+      $line_count++;
+      $line = trim($line);
+      
+      if( $line == '' ) {
+        //print '<p>Info: Line #' . $line_count . ': Blank line found. Skipping line</p>';
+        $skip_count++;
+        continue;
+      }
+      
+      if( preg_match('/^#/', $line) ) {
+        //print '<p>Info: Line #' . $line_count . ': Comment line found. Skipping line.</p>';
+        $skip_count++;
+        continue;
+      }
+
+      if( !preg_match('/' . $d . '/', $line) ) {
+        print '<p>Error: Line #' . $line_count . ': Deliminator (' .htmlentities($d) . ') Not Found. Skipping line.</p>';
+        $error_count++; $skip_count++;
+        continue;
+      }
+
+      $wp = explode($d, $line);
+
+      if( sizeof($wp) != 2 ) {
+        print '<p>Error: Line #' . $line_count . ': Malformed line.  Expecting 2 words, found ' . sizeof($wp) . ' words</p>';
+        $error_count++; $skip_count++;
+        continue;
+      }
+      
+      $sw = trim($wp[0]);
+      if( !$sw ) {
+        print '<p>Error: Line #' . $line_count . ': Malformed line.  Missing source word</p>';
+        $error_count++; $skip_count++;
+        continue;      
+      }
+      $tw = trim($wp[1]);
+      if( !$tw ) {
+        print '<p>Error: Line #' . $line_count . ': Malformed line.  Missing target word</p>';
+        $error_count++; $skip_count++;
+        continue;      
+      }
+      
+      $si = $this->get_id_from_word($sw);
+      $ti = $this->get_id_from_word($tw);
+      
+      $bind = array( 's_id'=>$si, 's_code'=>$s, 't_id'=>$ti, 't_code'=>$t);
+
+      // check if REVERSE pair already exists...
+      $sql = '
+        SELECT s_id 
+        FROM word2word
+        WHERE s_id = :t_id
+        AND t_id = :s_id
+        AND s_code = :t_code
+        AND t_code = :s_code
+        LIMIT 1
+      ';
+      $check = $this->db->query($sql,$bind);
+      if( $check ) {
+        //print '<p>Info: Line #' . $line_count . ': Duplicate (reverse). Skipping line';
+        $error_count++; $dupe_count++; $skip_count++;
+        continue;
+      }
+      
+      $sql = '
+        INSERT INTO word2word (
+          s_id, s_code, t_id, t_code
+        ) VALUES (
+          :s_id, :s_code, :t_id, :t_code
+        )';
+      $r = $this->db->queryb($sql, $bind);
+      if( !$r ) {
+        if( $this->db->db->errorCode() == '0000' ) {
+          //print '<p>Info: Line #' . $line_count . ': Duplicate.  Skipping line';
+          $error_count++; $dupe_count++; $skip_count++;
+          continue;
+        }
+        print '<p>Error: Line #' . $line_count . ': Database Insert Error.'
+        . ' sw: ' . htmlentities($sw)
+        . ' tw: ' . htmlentities($tw)
+        . ' Bind: ' . print_r($bind,1) 
+        . ' errorInfo: ' . print_r($this->db->db->errorInfo(),1) 
+        . '</p>';
+        $error_count++; $skip_count++;
+      } else {
+        $import_count++;
+        
+        if( $line_count % 100 == 0 ) {
+          print ' ' . $line_count . ' ';
+          @ob_flush(); flush();
+        } elseif( $line_count % 10 == 0 ) {
+          print '.';
+          @ob_flush(); flush();
+        }
+         
+      }
+
+    } // end foreach line
+    
+    print '</small><hr />';
+    print '<code>' . $import_count . '</code> translations imported.<br />';
+    print '<code>' . $error_count . '</code> errors.<br />';
+    print '<code>' . $dupe_count . '</code> duplicates/existing.<br />';
+    print '<code>' . $skip_count . '</code> lines skipped.<br />';
+    print '</div>';
+    
+  } // end do_import
 
 } // end class ote
