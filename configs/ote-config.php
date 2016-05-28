b265dd8d3f3c060e1c0f2c906b9916d779264230
David B <barberi.office@gmail.com>
2016-05-28 09:34:38 +0200

    force slash setup, exception for word action

--- configs/ote-config.php
+++ configs/ote-config.php
@@ -8,3 +8,9 @@ $config['depth']['dictionary'] = 4; // Dictionary Depth
 $config['depth']['word'] = 5; // word viewer Depth
 
 $config['depth']['export'] = 4; // Export Depth
+
+// By default, a slash / is forced onto the end of all URLs, if not already present.
+// To allow slash-less ending URLs, list the action name here:
+$config['force_slash_exceptions'] = array(
+  'word',
+);
