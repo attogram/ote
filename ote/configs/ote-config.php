f6bb9032cbc50eae789bcad330a6f2734ca6e559
David B <barberi.office@gmail.com>
2016-06-14 21:32:49 +0200

    force_slash_exceptions rename to no_end_slash

--- ote/configs/ote-config.php
+++ ote/configs/ote-config.php
@@ -9,6 +9,6 @@ $config['site_name'] = 'Open Translation Engine';
 $config['depth']['dictionary'] = 3; // Dictionary Depth
 
 $config['depth']['word'] = 4; // word viewer Depth
-$config['force_slash_exceptions'][] = 'word'; // no end slashes on word
+$config['no_end_slash'][] = 'word'; // no end slashes on word
 
 $config['depth']['export'] = 3; // Export Depth
