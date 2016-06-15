c7d84d9a008d4d95609a1939c8050dd50b14493f
David B <barberi.office@gmail.com>
2016-06-15 21:31:25 +0200

    styleing

--- ote/actions/history.php
+++ ote/actions/history.php
@@ -12,29 +12,26 @@ $sql = 'SELECT * FROM history ORDER BY date DESC, count DESC';
 $hr = $this->db->query($sql);
 
 print '<div class="row" style="padding:1px; font-weight:bold;">
-  <div class="col-xs-2">Word</div>
-  <div class="col-xs-1">#</div>
-  <div class="col-xs-3">Day</div>
-  <div class="col-xs-3"><small>Source Language</small></div>
-  <div class="col-xs-3"><small>Target Language</small></div>
+  <div class="col-xs-4">Word</div>
+  <div class="col-xs-1 small">#</div>
+  <div class="col-xs-3 small">Day</div>
+  <div class="col-xs-2 small">Source</div>
+  <div class="col-xs-2 small">Target</div>
 </div>';
 
 foreach( $hr as $h ) {
-  $url = $this->path . '/word///' . urlencode($h['word']);
+
   $sl_code = $ote->get_language_code_from_id($h['sl']);
-  if( !$sl_code ) {
-    $sl_code = '*';
-  }
   $tl_code = $ote->get_language_code_from_id($h['tl']);
-  if( !$tl_code ) {
-    $tl_code = '*';
-  }
+
+  $url = $this->path . '/word/' . $sl_code . '/' . $tl_code . '/' . urlencode($h['word']);
+
   print '<div class="row" style="border:1px solid #eeeeee; padding:0px;">
-    <div class="col-xs-2"><a href="' . $url . '">' . htmlentities($h['word']) . '</a></div>
-    <div class="col-xs-1">' . $h['count'] . '</div>
-    <div class="col-xs-3">' . $h['date'] . '</div>
-    <div class="col-xs-3">' . $sl_code . '</div>
-    <div class="col-xs-3">' . $tl_code. '</div>
+    <div class="col-xs-4"><a href="' . $url . '">' . htmlentities($h['word']) . '</a></div>
+    <div class="col-xs-1 small">' . $h['count'] . '</div>
+    <div class="col-xs-3 small">' . $h['date'] . '</div>
+    <div class="col-xs-2 small">' . ($sl_code ? $sl_code : '*') . '</div>
+    <div class="col-xs-2 small">' . ($tl_code ? $tl_code : '*'). '</div>
   </div>';
 }
 
