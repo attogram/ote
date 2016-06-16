162f706d2f70e60d66785bd704125fa26efe55ab
David B <barberi.office@gmail.com>
2016-06-16 17:13:36 +0200

    styleing

--- ote/actions/history.php
+++ ote/actions/history.php
@@ -1,8 +1,8 @@
-<?php // Open Translation Engine - History Page v0.0.2
+<?php // Open Translation Engine - History Page v0.0.3
 
 namespace Attogram;
 
-$ote = new ote($this->db, $this->log);
+$ote = new ote( $this->db, $this->log );
 
 $title = '🔭 History';
 $this->page_header($title);
@@ -12,9 +12,9 @@ $sql = 'SELECT * FROM history ORDER BY date DESC, count DESC';
 $hr = $this->db->query($sql);
 
 print '<div class="row" style="padding:1px; font-weight:bold;">
-  <div class="col-xs-4 col-sm-4">Word</div>
+  <div class="col-xs-4 col-sm-5">Word</div>
   <div class="col-xs-4 col-sm-1 small">#</div>
-  <div class="col-xs-4 col-sm-3 small">Day</div>
+  <div class="col-xs-4 col-sm-2 small">Day</div>
   <div class="clearfix visible-xs-block"></div>
   <div class="col-xs-4 col-sm-2 small">Source</div>
   <div class="col-xs-4 col-sm-2 small">Target</div>
@@ -30,9 +30,9 @@ foreach( $hr as $h ) {
   $url = $this->path . '/word/' . $sl_code . '/' . $tl_code . '/' . urlencode($h['word']);
 
   print '<div class="row" style="border:1px solid #eeeeee; padding:0px;">
-    <div class="col-xs-4 col-sm-4"><a href="' . $url . '"><h2 style="display:inline;">' . htmlentities($h['word']) . '</h2></a></div>
+    <div class="col-xs-4 col-sm-5"><a href="' . $url . '"><h2 style="display:inline;">' . htmlentities($h['word']) . '</h2></a></div>
     <div class="col-xs-4 col-sm-1 small">' . $h['count'] . '</div>
-    <div class="col-xs-4 col-sm-3 small">' . $h['date'] . '</div>
+    <div class="col-xs-4 col-sm-2 small">' . $h['date'] . '</div>
     <div class="clearfix visible-xs-block"></div>
     <div class="col-xs-4 col-sm-2 small">' . ($sl_name ? $sl_name : '*') . '</div>
     <div class="col-xs-4 col-sm-2 small">' . ($tl_name ? $tl_name : '*') . '</div>
