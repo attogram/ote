8146f6efcae61cc13a5726e518b7f8afb02fd778
David B <barberi.office@gmail.com>
2016-06-25 16:45:20 +0200

    implement delete option

--- actions/slush_pile.php
+++ actions/slush_pile.php
@@ -1,11 +1,37 @@
-<?php // Open Translation Engine - Slush Pile Page v0.0.6
+<?php // Open Translation Engine - Slush Pile Page v0.0.8
 
 namespace Attogram;
 
-$title = '🛃 Slush Pile';
+if( $_GET ) {
+  if(
+    !isset($_GET['a']) || !( $_GET['a'] == 'a' || $_GET['a'] == 'd' )
+    || !isset($_GET['i']) || !$_GET['i'] || !is_numeric($_GET['i'])
+  ) {
+    $this->error404('Slush pile options not slushable.');
+  }
+  $action = urldecode($_GET['a']);
+  $slush_id = urldecode($_GET['i']);
+  switch( $action ) {
+    case 'a':
+      print "<p>ACTION:$action slush_id:$slush_id sql:$sql bind:" . print_r($bind,1) . "<p>";
+      break;
+    case 'd':
+      $sql = 'DELETE FROM slush_pile WHERE id = :id';
+      $bind['id'] = $slush_id;
+      if( $this->db->queryb( $sql, $bind ) ) {
+        print '<div class="alert alert-success"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+        . '<strong>Deleted</strong> Slush Pile ID #' . htmlentities($slush_id) . '</div>';
+      } else {
+        print '<div class="alert alert-danger"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+        . '<strong>Error</strong> - can not delete Slush Pile ID #' . htmlentities($slush_id) . '</div>';      }
+      break;
+  }
+} // end if _GET
 
-$this->page_header($title);
-print '<div class="container"><h1 class="squished">' . $title . '</h1>';
+
+$this->page_header('🛃 Slush Pile');
+print '<div class="container">';
+print '<h1 class="squished"><a href="./" style="color:inherit;text-decoration:none;">🛃</a> Slush Pile</h1>';
 
 $ote = new ote( $this->db, $this->log );
 
@@ -30,7 +56,7 @@ print $this->db->pager(
 );
 
 foreach( $slush as $s ) {
-  //print '<pre>' . print_r($s,1) . '</pre>';
+  $id = isset($s['id']) ? $s['id'] : 0;
   $date = isset($s['date']) ? htmlentities($s['date']) : '';
   $type = isset($s['type']) ? htmlentities($s['type']) : '';
   $user_id = isset($s['user_id']) ? htmlentities($s['user_id']) : '';
@@ -41,10 +67,12 @@ foreach( $slush as $s ) {
   $target_word_url = '../word///' . urlencode($s['target_word']);
   $target_language_code = isset($s['target_language_code']) ? htmlentities($s['target_language_code']) : '';
   print '<div class="row" style="border:solid 1px #ccc;padding:4px;">'
-  . '<div class="col-sm-4">🕑:'  . $date . ' 👤:' . $user_id . ' 🔀:<strong>' . $type . '</strong></div>'
-  . '<div class="col-sm-3"><a href="' . $source_word_url . '">' . $source_word . '</a></div>'
-  . '<div class="col-sm-3"> = <a href="' . $target_word_url . '">' . $target_word . '</a></div>'
-  . '<div class="col-sm-2">' . "<code>$source_language_code</code> = <code>$target_language_code</code></div>"
+  . '<div class="col-sm-4">🕑:' . $date . ' 👤:' . $user_id . ' 🔀:<strong>' . $type . '</strong></div>'
+  . '<div class="col-sm-4"><a href="' . $source_word_url . '">' . $source_word . '</a> <code>' . $source_language_code . '</code>'
+  . ' = <a href="' . $target_word_url . '">' . $target_word . '</a> <code>' . $target_language_code . '</code></div>'
+  . '<div class="col-sm-4">'
+  . '<a href="?a=a&i=' . urlencode($id) . '">✔ Accept</a>'
+  . ' &nbsp; <a href="?a=d&i=' . urlencode($id) . '">✖ Delete</a></div>'
   . '</div>';
 }
 
