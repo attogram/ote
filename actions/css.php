0779e7150997e96a422ff4636d43cbce95e40c3e
David B <barberi.office@gmail.com>
2016-06-27 20:12:02 +0200

    test ote header + css as action (not cached?)

---
+++ actions/css.php
@@ -0,0 +1,7 @@
+<?php // Open Translation Engine - CSS v0.0.1
+
+header('Content-type: text/css');
+
+?>
+A:hover {   background-color:#ff9; color:#000;  }
+A:active {  background-color:#ff0; color:#000;  }
