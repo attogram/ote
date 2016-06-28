f50dd794d034eeb8ea2b1ae999f8fbf4375559b6
David B <barberi.office@gmail.com>
2016-06-28 09:11:27 +0200

    ote.css to new virtual web directory

--- actions/css.php
+++
@@ -1,7 +0,0 @@
-<?php // Open Translation Engine - CSS v0.0.1
-
-header('Content-type: text/css');
-
-?>
-A:hover {   background-color:#ff9; color:#000;  }
-A:active {  background-color:#ff0; color:#000;  }
--- templates/header.php
+++ templates/header.php
@@ -1,4 +1,4 @@
-<?php // Open Translation Engine - Page Header v0.0.1
+<?php // Open Translation Engine - Page Header v0.0.2
 
 namespace Attogram;
 
@@ -10,7 +10,7 @@ $title = 'Open Translation Engine';
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="<?php print $this->path; ?>/web/attogram.css">
-<link rel="stylesheet" href="<?php print $this->path; ?>/css/">
+<link rel="stylesheet" href="<?php print $this->path; ?>/web/ote.css">
  <!--[if lt IE 9]>
    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
