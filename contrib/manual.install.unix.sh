91d145e00e7a4a423a2a499a369071a2c7fe1693
David B <barberi.office@gmail.com>
2016-06-29 13:31:39 +0200

    install cache

--- contrib/manual.install.unix.sh
+++ contrib/manual.install.unix.sh
@@ -1,98 +1,108 @@
-echo "Open Translation Engine - Manual Install Script for unix platforms - v0.0.1"
-echo ----------------------
+echo "Open Translation Engine - Manual Install Script for unix platforms - v0.0.2"
 
-INSTALL_DIR="./ote-test"
+INSTALL_DIR="./ote-test" # no trailing slash
+INSTALL_CACHE="./install-cache" # no trailing slash
+
 ATTOGRAM_ZIP="https://github.com/attogram/attogram/archive/master.zip"
 ATTOGRAM_FILE="attogram.zip"
-ATTOGRAM_DIR="attogram-master"
+ATTOGRAM_DIR="attogram-master" # no trailing slash
+
 ATTOGRAM_VENDOR_ZIP="https://github.com/attogram/attogram-vendor/archive/master.zip"
 ATTOGRAM_VENDOR_FILE="attogram_vendor.zip"
-ATTOGRAM_VENDOR_DIR="attogram-vendor-master"
+ATTOGRAM_VENDOR_DIR="attogram-vendor-master" # no trailing slash
+
 OTE_ZIP="https://github.com/attogram/ote/archive/master.zip"
 OTE_FILE="ote.zip"
-OTE_DIR="ote-master"
-UNZIP="unzip -q"
-WGET="wget --no-verbose"
-CP="cp --recursive" # --verbose
+OTE_DIR="ote-master" # no trailing slash
 
-echo "Setup:"
-echo "INSTALL_DIR: " $INSTALL_DIR
-echo "Attogram Framework: zip: $ATTOGRAM_ZIP"
-echo "Attogram Framework: file: $ATTOGRAM_FILE"
-echo "Attogram Framework: dir: $ATTOGRAM_DIR"
-echo "Attogram Framework vendor: zip: $ATTOGRAM_VENDOR_ZIP"
-echo "Attogram Framework vendor: file: $ATTOGRAM_VENDOR_FILE"
-echo "Attogram Framework vendor: dir: $ATTOGRAM_VENDOR_DIR"
-echo "Open Translation Engine: zip: $OTE_ZIP"
-echo "Open Translation Engine: file: $OTE_FILE"
-echo "Open Translation Engine: dir: $OTE_DIR"
-echo "wget: $WGET"
-echo "unzip: $UNZIP"
-echo "cp: $CP"
+UNZIP="unzip" # -q
+WGET="wget" # --no-verbose
+CP="cp --recursive --verbose" # --verbose
+
+echo ----------------------
+if [ ! -d "$INSTALL_CACHE" ];
+then
+  echo "Creating install cache: $INSTALL_CACHE"
+  mkdir $INSTALL_CACHE
+fi
+echo "Install cache: $INSTALL_CACHE"
+if [ ! -d "$INSTALL_DIR" ];
+then
+  echo "Creating install directory: $INSTALL_DIR"
+  mkdir $INSTALL_DIR
+fi
+echo "Install directory: $INSTALL_DIR"
 
 echo ----------------------
 echo "Get Attogram Framework"
-if [ -r "$ATTOGRAM_FILE" ];
+if [ -r "$INSTALL_CACHE/$ATTOGRAM_FILE" ];
 then
-  echo "OK: $ATTOGRAM_FILE"
+  echo "OK: $INSTALL_CACHE/$ATTOGRAM_FILE"
 else
-  $WGET --output-document=$ATTOGRAM_FILE $ATTOGRAM_ZIP
+  $WGET --output-document=$INSTALL_CACHE/$ATTOGRAM_FILE $ATTOGRAM_ZIP
 fi
 echo "Get Attogram Framework Vendor package"
-if [ -r "$ATTOGRAM_VENDOR_FILE" ];
+if [ -r "$INSTALL_CACHE/$ATTOGRAM_VENDOR_FILE" ];
 then
-  echo "OK: $ATTOGRAM_VENDOR_FILE"
+  echo "OK: $INSTALL_CACHE/$ATTOGRAM_VENDOR_FILE"
 else
-  $WGET --output-document=$ATTOGRAM_VENDOR_FILE $ATTOGRAM_VENDOR_ZIP
+  $WGET --output-document=$INSTALL_CACHE/$ATTOGRAM_VENDOR_FILE $ATTOGRAM_VENDOR_ZIP
 fi
 echo "Get Open Translation Engine"
-if [ -r "$OTE_FILE" ];
+if [ -r "$INSTALL_CACHE/$OTE_FILE" ];
 then
-  echo "OK: $OTE_FILE"
+  echo "OK: $INSTALL_CACHE/$OTE_FILE"
 else
-  $WGET --output-document=$OTE_FILE $OTE_ZIP
+  $WGET --output-document=$INSTALL_CACHE/$OTE_FILE $OTE_ZIP
 fi
 
 echo ----------------------
 echo "Unzip Attogram Framework"
-if [ -d "$ATTOGRAM_DIR" ];
+if [ -d "$INSTALL_CACHE/$ATTOGRAM_DIR" ];
 then
-  echo "OK: $ATTOGRAM_DIR"
+  echo "OK: $INSTALL_CACHE/$ATTOGRAM_DIR"
 else
-  $UNZIP $ATTOGRAM_FILE
+  $UNZIP -d $INSTALL_CACHE $INSTALL_CACHE/$ATTOGRAM_FILE
 fi
 echo "Unzip Attogram Framework vendor"
-if [ -d "$ATTOGRAM_VENDOR_DIR" ];
+if [ -d "$INSTALL_CACHE/$ATTOGRAM_VENDOR_DIR" ];
 then
-  echo "OK: $ATTOGRAM_VENDOR_DIR"
+  echo "OK: $INSTALL_CACHE/$ATTOGRAM_VENDOR_DIR"
 else
-  $UNZIP $ATTOGRAM_VENDOR_FILE
+  $UNZIP -d $INSTALL_CACHE $INSTALL_CACHE/$ATTOGRAM_VENDOR_FILE
 fi
 echo "Unzip Open Translation Engine"
-if [ -d "$OTE_DIR" ];
+if [ -d "$INSTALL_CACHE/$OTE_DIR" ];
 then
-  echo "OK: $OTE_DIR"
+  echo "OK: $INSTALL_CACHE/$OTE_DIR"
 else
-  $UNZIP $OTE_FILE
+  $UNZIP -d $INSTALL_CACHE $INSTALL_CACHE/$OTE_FILE
 fi
+
 
 echo ----------------------
 echo "Install Attogram Framework Vendor package"
-$CP $ATTOGRAM_VENDOR_DIR/vendor $ATTOGRAM_DIR/vendor
+$CP $INSTALL_CACHE/$ATTOGRAM_VENDOR_DIR/vendor $INSTALL_CACHE/$ATTOGRAM_DIR/vendor
 echo "Installing Open Translation Engine module"
-$CP $OTE_DIR $ATTOGRAM_DIR/modules/ote
+$CP $INSTALL_CACHE/$OTE_DIR $INSTALL_CACHE/$ATTOGRAM_DIR/modules/ote
 echo "Creating Attogram config"
-$CP $ATTOGRAM_DIR/public/config.sample.php $ATTOGRAM_DIR/public/config.php
+$CP $INSTALL_CACHE/$ATTOGRAM_DIR/public/config.sample.php $INSTALL_CACHE/$ATTOGRAM_DIR/public/config.php
 echo "Making database directory writeable"
-chmod 777 $ATTOGRAM_DIR/db
+chmod 777 $INSTALL_CACHE/$ATTOGRAM_DIR/db
 
 echo ----------------------
-echo "Attogram Install Directory: ./$ATTOGRAM_DIR/"
-echo "Attogram Config: ./$ATTOGRAM_DIR/public/config.php"
-echo "Open Translation Engine module: ./$ATTOGRAM_DIR/modules/ote/"
+echo "Creating:"
+$CP $INSTALL_CACHE/$ATTOGRAM_DIR $INSTALL_DIR
+
+echo ----------------------
+echo "Initialization done."
+echo "Attogram Install Directory: $INSTALL_DIR"
+echo "Attogram Config: $INSTALL_DIR/public/config.php"
+echo "Open Translation Engine module: $INSTALL_DIR/modules/ote/"
 echo
-echo "TODO: edit ./$ATTOGRAM_DIR/public/.htaccess"
-echo "TODO: edit ./$ATTOGRAM_DIR/public/config.php"
+echo "TODO: set web server home as: $INSTALL_DIR/public"
+echo "TODO: edit $INSTALL_DIR/public/.htaccess"
+echo "TODO: edit $INSTALL_DIR/public/config.php"
 echo
 
 echo ----------------------
