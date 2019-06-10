2ed755b7832a8cb1435ef4d8ab01203a83ce1163
Attogram Project <attogram@users.noreply.github.com>
2019-06-10 12:43:44 +0200

    languages init

---
+++ database/migrations/2019_06_10_101520_languages.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types = 1);
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Languages extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(
+            'languages', 
+            function (Blueprint $table) {
+                $table->bigIncrements('id');
+                $table->string('code');
+                $table->string('name');
+                $table->timestamps();
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('languages');
+    }
+}
