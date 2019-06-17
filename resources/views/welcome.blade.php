266277c01eb7c258d08bbbd4ae6b53f40a76e9fd
Attogram Project <attogram@users.noreply.github.com>
2019-06-17 12:02:27 +0200

    hp test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,93 +1,44 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Open Translation Engine</title>
+<style>
+body {
+    background-color: #fff;
+    color: #000;
+    font-family: sans-serif;
+    margin: 20px;
+}
+</style>
+</head>
+<body>
+<h1>Open Translation Engine</h1>
+<h2>Development Branch v2-laravel</h2>
+<h3>v2.0.0-alpha.11</h3>
 
-        <title>Open Translation Engine</title>
+<p>System:</p>
+<ul>
+@auth
+    <li><a href="{{ url('/home') }}">Home</a></li>
+@else
+    <li><a href="{{ route('login') }}">Login</a></li>
+    @if (Route::has('register'))
+    <li><a href="{{ route('register') }}">Register</a></li>
+    @endif
+@endauth
+</ul>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<p>Info:</p>
+<ul>
+    <li><a href="https://github.com/attogram/ote">https://github.com/attogram/ote</a></li>
+    <li><a href="https://attogram.github.io/ote/">https://attogram.github.io/ote</a></li>
+</ul>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-
-            <div class="top-right links">
-                @auth
-                    <a href="{{ url('/home') }}">Home</a>
-                @else
-                    <a href="{{ route('login') }}">Login</a>
-
-                    @if (Route::has('register'))
-                        <a href="{{ route('register') }}">Register</a>
-                    @endif
-                @endauth
-            </div>
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Open Translation Engine v2.0.0-alpha.10
-                </div>
-
-                <div class="links">
-                    <a href="https://github.com/attogram/ote">https://github.com/attogram/ote</a>
-                    <a href="https://attogram.github.io/ote/">https://attogram.github.io/ote</a>
-                </div>
-            </div>
-        </div>
-    </body>
+<p>Debug:</p>
+<pre>
+locale: <?= app()->getLocale(); ?> 
+</pre>
+</body>
 </html>
